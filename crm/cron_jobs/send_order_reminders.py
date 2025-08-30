c016388dc6efffb5a2ddb06b124eaf121d9a12d7
Eddy Ckedar <eddykedar@gmail.com>
2025-08-30 19:23:27 +0200

    Create send_order_reminders.py

---
+++ crm/cron_jobs/send_order_reminders.py
@@ -0,0 +1,30 @@
+import datetime
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+
+# Setup GraphQL client
+transport = RequestsHTTPTransport(url="http://localhost:8000/graphql", verify=False)
+client = Client(transport=transport, fetch_schema_from_transport=True)
+
+# Calculate cutoff date (last 7 days)
+cutoff_date = (datetime.datetime.now() - datetime.timedelta(days=7)).strftime("%Y-%m-%d")
+
+query = gql(f"""
+query {{
+  orders(filter: {{ orderDate_Gte: "{cutoff_date}" }}) {{
+    id
+    customer {{
+      email
+    }}
+  }}
+}}
+""")
+
+result = client.execute(query)
+orders = result.get("orders", [])
+
+with open("/tmp/order_reminders_log.txt", "a") as log:   # <-- fixed log filename
+    for order in orders:
+        log.write(f"{datetime.datetime.now()} - Order {order['id']} - {order['customer']['email']}\n")
+
+print("Order reminders processed!")
