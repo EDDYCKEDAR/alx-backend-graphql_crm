800860f919e1eee40ceee10c48f02676f1b380ed
Eddy Ckedar <eddykedar@gmail.com>
2025-08-30 19:35:02 +0200

    Update cron.py

--- crm/cron.py
+++ crm/cron.py
@@ -1,22 +1,26 @@
-from datetime import datetime
+import datetime
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 
-def log_crm_heartbeat():
-    now = datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
-
+def update_low_stock():
     # Setup GraphQL client
     transport = RequestsHTTPTransport(url="http://localhost:8000/graphql", verify=False)
     client = Client(transport=transport, fetch_schema_from_transport=True)
 
-    # Try querying the hello field
-    try:
-        query = gql(""" query { hello } """)
-        result = client.execute(query)
-        hello_response = result.get("hello", "No response")
-    except Exception as e:
-        hello_response = f"GraphQL error: {e}"
+    # Correct mutation name: updateLowStockProducts
+    mutation = gql("""
+    mutation {
+      updateLowStockProducts {
+        success
+        updated
+      }
+    }
+    """)
 
-    # Log heartbeat with GraphQL response
-    with open("/tmp/crm_heartbeat_log.txt", "a") as f:
-        f.write(f"{now} CRM is alive - Hello field: {hello_response}\n")
+    result = client.execute(mutation)
+    updates = result["updateLowStockProducts"]["updated"]
+
+    # Correct log path: /tmp/low_stock_updates_log.txt
+    with open("/tmp/low_stock_updates_log.txt", "a") as f:
+        for line in updates:
+            f.write(f"{datetime.datetime.now()} - {line}\n")
